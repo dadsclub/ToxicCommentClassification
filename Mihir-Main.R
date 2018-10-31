8d979c1ae9e69ec413608d2f05bcda71f8e75359
Mihir Lad <mihirlad55@gmail.com>
2018-10-31 13:41:54 -0400

    Added Lots of Plots and Some Feature Engineering Feature Engineering: 	- Converted comment_text from factor into character array 	- Created tidy_train with individual words rather than comments 	- Removed stop_words from tidy_train 	- Removed observations without any classifications 	- Converted word column into factor Plots: 	- Added plots for Top 10 Words Within Comments Flagged by each classification type 	- Added grid view of these plots

--- Mihir-Main.R
+++ Mihir-Main.R
@@ -4,16 +4,19 @@ dev.off(dev.list()["RStudioGD"])
 
 
 # Import Libraries
-library(tidyverse)
+library(tidytext)
 library(dplyr)
 library(ggplot2)
-library(ggpubr)
+library(stringr)
+library(gridExtra)
 
 
 # Import data into environment if not already imported
 if (!exists("train")) {
   assign("train", read.csv("data/train.csv"))
 }
+
+train$comment_text <- as.character(train$comment_text)
 
 # Turn Binary Columns into Logical Columns
 train$toxic = as.logical(train$toxic)
@@ -43,7 +46,21 @@ ClassificationSummary$F <- t(train %>%
               nIdentityHate = n() - sum(identity_hate)))
 colnames(ClassificationSummary) <- c("T", "F")
 
+remove(F)
+remove(T)
 
+
+# Split comments into separate rows with separate words and remove stop words
+tidy_train <- train %>%
+unnest_tokens(word, comment_text) %>%
+  anti_join(stop_words)
+
+# Remove observations with no bad comments
+tidy_train <- tidy_train %>%
+  filter(toxic + severe_toxic + obscene + threat + insult + identity_hate > 0)
+
+# Make words into factors
+tidy_train$word <- as.factor(tidy_train$word)
 
 # Plot Bar Graph of each type of classification
 ClassificationSummary %>%
@@ -53,3 +70,90 @@ ClassificationSummary %>%
        x = "Classification",
        y = "Number of Comments")
 
+
+
+# Plot most common words within comments flagged as toxic
+(plotTop10Toxic <- tidy_train %>%
+  group_by(word) %>%
+  filter(toxic == TRUE) %>%
+  summarise(NumOfOccurances = n()) %>%
+  arrange(desc(NumOfOccurances)) %>%
+  head(10) %>%
+  ggplot() +
+    geom_bar(mapping = aes(x = word, y = NumOfOccurances), stat = "identity") +
+    labs(title = "Top 10 Words Within Comments Flagged as Toxic",
+         x = "Word",
+         y = "Number of Occurances"))
+
+
+# Plot most common words within comments flagged as severe_toxic
+(plotTop10SevereToxic <- tidy_train %>%
+  group_by(word) %>%
+  filter(severe_toxic == TRUE) %>%
+  summarise(NumOfOccurances = n()) %>%
+  arrange(desc(NumOfOccurances)) %>%
+  head(10) %>%
+  ggplot() +
+  geom_bar(mapping = aes(x = word, y = NumOfOccurances), stat = "identity") +
+  labs(title = "Top 10 Words Within Comments Flagged as Severe Toxic",
+       x = "Word",
+       y = "Number of Occurances"))
+
+
+# Plot most common words within comments flagged as obscene
+(plotTop10Obscene <- tidy_train %>%
+  group_by(word) %>%
+  filter(obscene == TRUE) %>%
+  summarise(NumOfOccurances = n()) %>%
+  arrange(desc(NumOfOccurances)) %>%
+  head(10) %>%
+  ggplot() +
+  geom_bar(mapping = aes(x = word, y = NumOfOccurances), stat = "identity") +
+  labs(title = "Top 10 Words Within Comments Flagged as Obscene",
+       x = "Word",
+       y = "Number of Occurances"))
+
+
+# Plot most common words within comments flagged as threat
+(plotTop10Threat <- tidy_train %>%
+  group_by(word) %>%
+  filter(threat == TRUE) %>%
+  summarise(NumOfOccurances = n()) %>%
+  arrange(desc(NumOfOccurances)) %>%
+  head(10) %>%
+  ggplot() +
+  geom_bar(mapping = aes(x = word, y = NumOfOccurances), stat = "identity") +
+  labs(title = "Top 10 Words Within Comments Flagged as Threat",
+       x = "Word",
+       y = "Number of Occurances"))
+
+
+# Plot most common words within comments flagged as Insult
+(plotTop10Insult <- tidy_train %>%
+  group_by(word) %>%
+  filter(insult == TRUE) %>%
+  summarise(NumOfOccurances = n()) %>%
+  arrange(desc(NumOfOccurances)) %>%
+  head(10) %>%
+  ggplot() +
+  geom_bar(mapping = aes(x = word, y = NumOfOccurances), stat = "identity") +
+  labs(title = "Top 10 Words Within Comments Flagged as Insult",
+       x = "Word",
+       y = "Number of Occurances"))
+
+
+# Plot most common words within comments flagged as identity_hate
+(plotTop10IdentityHate <- tidy_train %>%
+  group_by(word) %>%
+  filter(identity_hate == TRUE) %>%
+  summarise(NumOfOccurances = n()) %>%
+  arrange(desc(NumOfOccurances)) %>%
+  head(10) %>%
+  ggplot() +
+  geom_bar(mapping = aes(x = word, y = NumOfOccurances), stat = "identity") +
+  labs(title = "Top 10 Words Within Comments Flagged as Identity Hate",
+       x = "Word",
+       y = "Number of Occurances"))
+
+# Plot most common words within comments flagged as each classification in a grid
+grid.arrange(plotTop10Toxic, plotTop10SevereToxic, plotTop10Obscene, plotTop10Threat, plotTop10Insult, plotTop10IdentityHate)
